3b23dca1a6f10e1e67b8121c1f2cac629536f373
lindsay culver & jessica joly <student@epicodus.com>
2015-09-08 14:05:42 -0700

    All specs pass for all three functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
-
-var triangleTracker = function(side1,side2,side3) {
-
-
 var equilateral = function(side1,side2,side3) {
  if ((side1 === side2) && (side1 === side3) && (side3 === side2)) {
   return true;
  } else  {
+   return false;
+ }
 };
+
 
 var isoceles = function(side1,side2,side3) {
  if ((side1 === side2) || (side1 === side3) || (side3 === side2)) {
   return true;
  } else {
+   return false;
+ }
 };
 
 var scalene = function(side1,side2,side3) {
@@ -19,6 +20,5 @@ var scalene = function(side1,side2,side3) {
   return true;
   } else {
   return false;
-};
-
+ }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,17 @@
-describe('triangleTracker', function() {
-  it("is true if it is a triangle", function() {
-    expect(triangleTracker(2,3,4)).to.equal(true);
-  });
-
+describe('equilateral', function() {
   it("is true if it is an equilateral", function() {
-    expect(triangleTracker(4, 4, 4)).to.equal(true);
+    expect(equilateral(4, 4, 4)).to.equal(true);
   });
+});
 
+describe('isosceles', function() {
   it("is true if it is an isosceles", function() {
-    expect(triangleTracker(2, 2, 4)).to.equal(true);
+    expect(isoceles(2, 2, 4)).to.equal(true);
   });
+});
 
+describe('scalene', function() {
   it("is true if it is a scalene triangle", function() {
-    expect(triangleTracker(2, 1, 3)).to.equal(true);
+    expect(scalene(2, 1, 3)).to.equal(true);
   });
 });
